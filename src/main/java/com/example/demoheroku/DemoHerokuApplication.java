935f21ad993870d1d93fbd630f3ef3f58492dc1a
trongdat1998 <68001618+trongdat1998@users.noreply.github.com>
2021-10-20 00:36:44 +0700

    Initial commit

---
+++ src/main/java/com/example/demoheroku/DemoHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHerokuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHerokuApplication.class, args);
+    }
+
+}
