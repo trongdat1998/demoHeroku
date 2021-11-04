a66c80e89b38ee512f473f1531be9d625ae5f0f0
trongdat1998 <68001618+trongdat1998@users.noreply.github.com>
2021-11-04 23:57:24 +0700

    heroku

--- src/main/java/com/example/demoheroku/controller/HomeController.java
+++ src/main/java/com/example/demoheroku/controller/HomeController.java
@@ -2,18 +2,23 @@ package com.example.demoheroku.controller;
 
 import com.example.demoheroku.dao.AccountDao;
 import com.example.demoheroku.entity.Account;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
-
 @Controller
 public class HomeController {
-    @RequestMapping("/home")
-    public String heroku(){
-        return "index.html";
+    @Autowired
+    AccountDao ac;
+    @GetMapping("/demo")
+    public String heroku(Model model){
+        List<Account> a = ac.findAll();
+      model.addAttribute("mesage",a.get(0).getUsername());
+        return "heroku";
     }
 }
